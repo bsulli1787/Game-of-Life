f4c0cff050af7cad95fa8a901f140d1a07e5bcca
Braden Sullivan <bradensullivan@hotmail.com>
2021-11-20 09:13:19 -0400

    beginning work on game sum

--- Game of Life/Game_of_Life.py
+++ Game of Life/Game_of_Life.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 
-initial_matrix = np.random.randn(100,100)
+input_size = 100
+
+initial_matrix = np.random.randn(input_size + 2, input_size + 2)
 initial_matrix = [i >= 0.5 for i in initial_matrix]
 
 df_layout = pd.DataFrame(initial_matrix, dtype = int)
-
+df_cycle = pd.DataFrame().reindex_like(df_layout)
+df_cycle.iloc[1:input_size - 1, 1:input_size - 1] = df_layout.loc[i - 1: i + 1, i - 1: i + 1].sum()
 print(df_layout)
 
 
