8cee4333c3709693b2859ad161ebdc8a7912f9d1
Braden Sullivan <bradensullivan@hotmail.com>
2021-11-20 08:02:30 -0400

    intial setup, create a random array of 1s and 0s

--- Game of Life/Game_of_Life.py
+++ Game of Life/Game_of_Life.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import numpy as np
+
+initial_matrix = np.random.randn(100,100)
+initial_matrix = [i >= 0.5 for i in initial_matrix]
+
+df_layout = pd.DataFrame(initial_matrix, dtype = int)
+
+print(df_layout)
+
 
