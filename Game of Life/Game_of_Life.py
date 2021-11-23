538944a79286d731ffbe4b5774c29c8c80aabc23
Braden Sullivan <bradensullivan@hotmail.com>
2021-11-23 06:22:12 -0400

    trying it with an array instead of pandas

--- Game of Life/Game_of_Life.py
+++ Game of Life/Game_of_Life.py
@@ -3,12 +3,16 @@ import numpy as np
 
 input_size = 100
 
-initial_matrix = np.random.randn(input_size + 2, input_size + 2)
-initial_matrix = [i >= 0.5 for i in initial_matrix]
-
-df_layout = pd.DataFrame(initial_matrix, dtype = int)
-df_cycle = pd.DataFrame().reindex_like(df_layout)
-df_cycle.iloc[1:input_size - 1, 1:input_size - 1] = df_layout.loc[i - 1: i + 1, i - 1: i + 1].sum()
-print(df_layout)
+initial_array = np.random.randn(input_size + 2, input_size + 2)
+initial_array = [i >= 0.5 for i in initial_array]
+cycle_array = []
+#df_layout = pd.DataFrame(initial_matrix, dtype = int)
+#df_cycle = pd.DataFrame().reindex_like(df_layout)
+for j in range(1, np.shape(initial_array)[0] - 1):
+    for i in range(1, np.shape(initial_array)[1] - 1):
+        cycle_array[i,j] = sum(initial_array[i - 1: i + 1][j - 1: j + 1])
+        i += 1
+    j+=1
+print(cycle_array)
 
 
